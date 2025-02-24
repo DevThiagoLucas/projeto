2f22a5d9c2259667bafc7ca1a01cbc63d6cdfeba
Thiago Lucas <lucasthiago553@gmail.com>
2025-02-24 14:58:00 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement
+  //Realiza a troca de temas
+  html.classList.toggle("light")
+  // Pega a tag img
+  const img = document.querySelector("#profile img")
+  //Realiza a troca da imagem quando troca o tema
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/Avatar_2.png")
+  } else {
+    img.setAttribute("src", "./assets/Avatar.png")
+  }
+  
+}
